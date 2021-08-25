1acface938f81ca5f8ab813ec36686323df00572
FerCajas <87354391+FerCajas@users.noreply.github.com>
2021-08-25 02:41:26 -0500

    Add files via upload

---
+++ Api.js
@@ -0,0 +1,11 @@
+function iniciarMap(){
+    var coord = {lat:35.640462 ,lng: 139.780831};
+    var map = new google.maps.Map(document.getElementById('map'),{
+      zoom: 10,
+      center: coord
+    });
+    var marker = new google.maps.Marker({
+      position: coord,
+      map: map
+    });
+}
